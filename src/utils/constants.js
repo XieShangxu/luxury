264d465c785eb6fec730d8ef597e34b12a5f9b9c
Danie Xie <daniel.xie@farfetch.com>
2018-11-22 17:45:00 +0800

    add brand story

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 const constants = {
-  API_HOST: 'https://farfetch.danielxie.cn'
+  API_HOST: 'https://farfetch.danielxie.cn',
+  FF_API_HOST: 'https://westore-api.farfetch.cn'
 }
 
 export default constants
